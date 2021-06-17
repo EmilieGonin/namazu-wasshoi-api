3b1455f5cc362ed90243dccf79ee9ff7f3e8afc8
EmilieGonin <emilie.yuu@gmail.com>
2021-06-17 15:56:25 +0200

    build(deps): add cors

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || "3000";
 
+//Hello World Test
 app.get("/", (req, res) => {
 	res.send("Hello World");
 })
 
+app.use(cors({
+	credentials: true
+}));
+
+app.use(express.json());
 app.use("/user", require("./routes/user"));
 app.use("/fc", require("./routes/fc"));
 
