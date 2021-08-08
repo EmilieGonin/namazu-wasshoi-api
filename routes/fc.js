4bd3333a403405b2e32d6683b48c41d6c8052eb9
EmilieGonin <emilie.yuu@gmail.com>
2021-08-08 14:09:49 +0200

    feat: add condition to check if character is FC's member

--- routes/fc.js
+++ routes/fc.js
@@ -25,16 +25,18 @@ router.post("/character", (req, res, next) => {
   .then((response) => {
     const character = response.Results[0];
 
-    if (character) {
+    if (character && req.body.cl) {
       xivapi.character.get(character.ID, {data: "FC"})
       .then((response) => {
         const fc = response.FreeCompany;
         if (fc && fc.ID == fcId) {
           res.status(200).json({ character });
         } else {
-          res.status(404).json({ error: "Personnage non trouvé." });
+          res.status(404).json({ error: "Le personnage ne fait pas partie de Namazu Wasshoi." });
         }
       })
+    } else if (character) {
+      res.status(200).json({ character });
     } else {
       res.status(404).json({ error: "Personnage non trouvé." });
     }
