fe0060d7bb54ba2d97bdf93c1659efcba386cecd
EmilieGonin <emilie.yuu@gmail.com>
2021-08-14 15:49:46 +0200

    feat: remove sequelize (preparing sequelize migrations)

--- helpers/sequelize.js
+++ helpers/sequelize.js
@@ -16,27 +16,27 @@ const Screenshot = require("../models/Screenshot")(sequelize);
 const Parameter = require("../models/Parameter")(sequelize);
 
 //Site default parameters
-const parameters = [
-  { name: "recruiting", data: "true" }
-]
-const teams = [
-  { name: "Mog", slogan: "Pas de coups sous le pompon !" },
-  { name: "Chocobo", slogan: "On est chaud... cobo !" },
-  { name: "Pampa", slogan: "Qui s'y frotte s'y pique !" },
-  { name: "Carbuncle", slogan: "Que la poussière de diamant vous réduise en éclat !" }
-]
-const rosters= [
-  { name: "Pleine Lune", startHour: "16:00", endHour: "19:00" },
-  { name: "Nouvelle Lune", startHour: "21:00", endHour: "00:00" },
-  { name: "Croissant de Lune", startHour: "21:00", endHour: "23:30" }
-]
+// const parameters = [
+//   { name: "recruiting", data: "true" }
+// ]
+// const teams = [
+//   { name: "Mog", slogan: "Pas de coups sous le pompon !" },
+//   { name: "Chocobo", slogan: "On est chaud... cobo !" },
+//   { name: "Pampa", slogan: "Qui s'y frotte s'y pique !" },
+//   { name: "Carbuncle", slogan: "Que la poussière de diamant vous réduise en éclat !" }
+// ]
+// const rosters= [
+//   { name: "Pleine Lune", startHour: "16:00", endHour: "19:00" },
+//   { name: "Nouvelle Lune", startHour: "21:00", endHour: "00:00" },
+//   { name: "Croissant de Lune", startHour: "21:00", endHour: "23:30" }
+// ]
 
 Team.hasMany(User, { sourceKey: "name", foreignKey: "team" });
 Team.belongsTo(User, {
   scope: { isAdmin: true }, as: "Leader", constraints: false });
 User.belongsTo(Team, { targetKey: "name", foreignKey: "team" });
 
-Roster.hasMany(User, { sourceKey: "name", foreignKey: "roster" });
+Roster.hasMany(User, { sourceKey: "name", foreignKey: "roster" })
 User.belongsTo(Roster, { as: "Tank", targetKey: "name", foreignKey: "roster" });
 User.belongsTo(Roster, { as: "Healer", targetKey: "name", foreignKey: "roster" });
 User.belongsTo(Roster, { as: "DPS", targetKey: "name", foreignKey: "roster" });
@@ -64,104 +64,64 @@ sequelize.authenticate()
 .then(() => console.log("Connexion à la base de données MySQL terminée !"))
 .catch((error) => console.error("Impossible de se connecter à la base de données :", error));
 
-sequelize.sync({ force: true })
-.then(() => {
-  console.log("Base de données synchronisée avec succès !");
-
-  //Create admin user
-  User.findOrCreate({
-    where: {
-      email: process.env.ADMIN_EMAIL
-    },
-    defaults: {
-      character: process.env.ADMIN_CHARACTER,
-      characterId: process.env.ADMIN_CHARACTER_ID,
-      email: process.env.ADMIN_EMAIL,
-      password: process.env.ADMIN_PASSWORD,
-      isAdmin: true
-    }
-  })
-
-  //Create Parameters
-  for (const parameter of parameters) {
-    Parameter.findOrCreate({
-      where: {
-        name: parameter.name
-      },
-      defaults: {
-        name: parameter.name,
-        data: parameter.data
-      }
-    })
-  }
-
-  //Create Teams
-  for (const team of teams) {
-    Team.findOrCreate({
-      where: {
-        name: team.name
-      },
-      defaults: {
-        name: team.name,
-        slogan: team.slogan
-      }
-    })
-  }
-
-  //Create Rosters
-  for (const roster of rosters) {
-    Roster.findOrCreate({
-      where: {
-        name: roster.name
-      },
-      defaults: {
-        name: roster.name,
-        startHour: roster.startHour,
-        endHour: roster.endHour
-      }
-    })
-  }
-
-  //Applicant for testing
-  Applicant.create({
-    name: "Emilie",
-    birthday: "1996-05-27",
-    discord: "Yuuna#5839",
-    mic: true,
-    availability: process.env.LOREM,
-    about: "Test",
-    character: "Yuuna Tsukisagi",
-    characterId: 12177359,
-    mainClass: process.env.LOREM,
-    playtime: process.env.LOREM,
-    gameActivities: process.env.LOREM,
-    cl: process.env.LOREM,
-    clRequired: process.env.LOREM,
-    currentCl: process.env.LOREM,
-    exp: process.env.LOREM,
-    savageRequired: process.env.LOREM,
-    team: "Carbuncle"
-    })
-    Applicant.create({
-      name: "Laurent",
-      birthday: "1995-06-26",
-      discord: "Rabyte#9615",
-      mic: false,
-      availability: process.env.LOREM,
-      about: "Test",
-      character: "Rabyte Tsukisagi",
-      characterId: 12177390,
-      mainClass: process.env.LOREM,
-      playtime: process.env.LOREM,
-      gameActivities: process.env.LOREM,
-      cl: process.env.LOREM,
-      clRequired: process.env.LOREM,
-      currentCl: process.env.LOREM,
-      exp: process.env.LOREM,
-      savageRequired: process.env.LOREM,
-      team: "Pampa"
-      })
-})
-.catch((error) => console.error(error));
+// sequelize.sync()
+// .then(() => {
+//   console.log("Base de données synchronisée avec succès !");
+//
+//   //Create admin user
+//   User.findOrCreate({
+//     where: {
+//       email: process.env.ADMIN_EMAIL
+//     },
+//     defaults: {
+//       character: process.env.ADMIN_CHARACTER,
+//       characterId: process.env.ADMIN_CHARACTER_ID,
+//       email: process.env.ADMIN_EMAIL,
+//       password: process.env.ADMIN_PASSWORD,
+//       isAdmin: true
+//     }
+//   })
+//
+//   //Create Parameters
+//   for (const parameter of parameters) {
+//     Parameter.findOrCreate({
+//       where: {
+//         name: parameter.name
+//       },
+//       defaults: {
+//         name: parameter.name,
+//         data: parameter.data
+//       }
+//     })
+//   }
+//
+//   //Create Teams
+//   for (const team of teams) {
+//     Team.findOrCreate({
+//       where: {
+//         name: team.name
+//       },
+//       defaults: {
+//         name: team.name,
+//         slogan: team.slogan
+//       }
+//     })
+//   }
+//
+//   //Create Rosters
+//   for (const roster of rosters) {
+//     Roster.findOrCreate({
+//       where: {
+//         name: roster.name
+//       },
+//       defaults: {
+//         name: roster.name,
+//         startHour: roster.startHour,
+//         endHour: roster.endHour
+//       }
+//     })
+//   }
+// })
+// .catch((error) => console.error(error));
 
 module.exports = { Applicant, Team, User, Festival, Screenshot, Parameter }
