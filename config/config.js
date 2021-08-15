b727ab4b0a9f873f5f56724abb1ad7e08f942cf5
EmilieGonin <emilie.yuu@gmail.com>
2021-08-15 21:57:10 +0200

    feat: add logging: false to sequelize config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "url": process.env.LOCAL_DATABASE_URL,
-    "dialect": "mysql"
+  development: {
+    url: process.env.LOCAL_DATABASE_URL,
+    dialect: "mysql",
+    logging: false
   },
-  "production": {
-    "url": process.env.CLEARDB_DATABASE_URL,
-    "dialect": "mysql"
+  production: {
+    url: process.env.CLEARDB_DATABASE_URL,
+    dialect: "mysql",
+    logging: false
   }
 }
