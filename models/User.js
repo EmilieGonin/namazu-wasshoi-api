1fba438128e389fe49a0205db01c9aeddc0ef6e7
EmilieGonin <emilie.yuu@gmail.com>
2021-08-16 19:13:19 +0200

    feat: add beforeUpdate hook to hash new password if changed

--- models/User.js
+++ models/User.js
@@ -74,5 +74,10 @@ module.exports = (sequelize, DataTypes) => {
   User.beforeCreate(async (user) => {
     user.password = await bcrypt.hash(user.password, 10);
   })
+  User.beforeUpdate(async (user) => {
+    if (user.changed("password")) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  })
   return User;
 };
