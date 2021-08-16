42349b9b4351e0e778185fe473aee2434b47b947
EmilieGonin <emilie.yuu@gmail.com>
2021-08-17 01:18:58 +0200

    feat: add another festival on test seed

--- seeders/20210816221111-festival-test.js
+++ seeders/20210816221111-festival-test.js
@@ -3,16 +3,22 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("Festivals", [{
+      theme: "Soirée chic et cocktail",
+      start_date: "2021-06-28 08:00",
+      vote_date: "2021-07-30 20:00",
+      end_date: "2021-08-02 07:59"
+    }, {
       theme: "Plage paradisiaque",
-      start_date: "2021-08-01 08:00",
+      start_date: "2021-08-02 08:00",
       vote_date: "2021-08-27 20:00",
       end_date: "2021-08-29 20:00"
     }])
   },
 
   down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
     await queryInterface.bulkDelete("Festivals", [{
-      theme: "Plage paradisiaque"
+      theme: ["Soirée chic et cocktail", "Plage paradisiaque"]
     }]);
   }
 };
