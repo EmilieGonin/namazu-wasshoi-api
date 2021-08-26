1043ea9f73ad86d5ae234d6d261b7014d8094670
EmilieGonin <emilie.yuu@gmail.com>
2021-08-26 18:49:22 +0200

    refactor: add models with lowercase names

---
+++ models/applicant.js
@@ -0,0 +1,98 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Applicant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Applicant.init({
+    name: DataTypes.STRING,
+    birthday: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    discord: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    mic: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    availability: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    about: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    character: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    characterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    msq: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    mainClass: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    playtime: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    gameActivities: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    cl: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    clRequired: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    currentCl: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    exp: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    savageRequired: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    team: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    isArchived: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Applicant',
+  });
+  return Applicant;
+};
---
+++ models/festival.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Festival extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Festival.hasMany(models.Screenshot, { sourceKey: "edition", foreignKey: "festival" });
+      Festival.hasMany(models.Screenshot.scope("winner"), { as: "winners", sourceKey: "edition", foreignKey: "festival" });
+    }
+  };
+  Festival.init({
+    edition: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    theme: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    start_date: {
+      type: DataTypes.DATE,
+      unique: true
+    },
+    vote_date: {
+      type: DataTypes.DATE,
+      unique: true
+    },
+    end_date: {
+      type: DataTypes.DATE,
+      unique: true
+    },
+  }, {
+    sequelize,
+    modelName: 'Festival',
+    timestamps: false
+  });
+  return Festival;
+};
