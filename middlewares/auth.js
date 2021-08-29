2e0024f58707195a46741cf9295ed7cbb88b80e9
EmilieGonin <emilie.yuu@gmail.com>
2021-08-29 23:27:11 +0200

    feat: use new handling error route on auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,32 +2,32 @@ const { User } = require("../models/index");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  try {
-    //Check for headers authorization
-    if (!req.headers.authorization) {
-      throw "Authentification requise.";
+  const error = {
+    status: 401,
+    message: "Vous n'avez pas l'autorisation d'accéder à cette page."
+  }
+
+  //Check for headers authorization
+  if (!req.headers.authorization) {
+    next(error);
+  }
+
+  //Get user id from the authorization token
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  const userId = decodedToken.userId;
+
+  //Check if user exists on the database
+  User.findByPk(userId)
+  .then((user) => {
+    if (user) {
+      //Save user id on the request if needed
+      // req.userId = userId;
+      next();
     }
-
-    //Get user id from the authorization token
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const userId = decodedToken.userId;
-
-    //Check if user exists on the database
-    User.findByPk(userId)
-    .then((user) => {
-      if (user) {
-        //Save user id on the request if needed
-        // req.userId = userId;
-        next();
-      }
-      else {
-        throw "Utilisateur inexistant.";
-      }
-    })
-    .catch((error) => res.status(404).json({ error: error }));
-  }
-  catch(e) {
-    res.status(401).json({ error: e });
-  }
+    else {
+      next(error);
+    }
+  })
+  .catch((e) => next(e));
 };
