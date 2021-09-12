b39e5415a7ee7ea0f5aca134ca5247b535b89f26
EmilieGonin <emilie.yuu@gmail.com>
2021-09-12 02:59:30 +0200

    feat: improve applicant creation + get staff mails on database

--- routes/applicants.js
+++ routes/applicants.js
@@ -1,11 +1,11 @@
-const { Applicant, Profile, Character } = require("../models/index");
+const { Applicant, Profile, Character, User } = require("../models/index");
 const { transport, mailTemplate } = require("../helpers/nodemailer");
 const express = require('express');
 const router = express.Router();
 const auth = require("../middlewares/auth");
 
 //Get all applicants
-router.get("/", auth, (req, res, next) => {
+router.get("/", auth, async (req, res, next) => {
   Applicant.findAll({
     include: [{
       model: Profile,
@@ -19,32 +19,36 @@ router.get("/", auth, (req, res, next) => {
 });
 
 //Post new applicant
-router.post("/new", (req, res, next) => {
-  const applicant = {}
-
-  for (const data of Object.entries(req.body)) {
-    const name = data[0];
-    let value = data[1];
-    if (value == "true" || value == "false") {
-      value = !!value;
+router.post("/new", async (req, res, next) => {
+  const applicant = await Applicant.create(req.body, {
+    include: [ Profile, Character ]
+  }).catch(() => {
+    const error = {
+      status: 400,
+      message: "Une erreur s'est produite. Si vous aviez déjà postulé, vous ne pourrez pas le faire une seconde fois."
     }
-    applicant[name] = value;
+    next(error);
+  })
+
+  const staffMails = [];
+  const staff = await User.findAll({
+    where: { isAdmin: true },
+    attributes: ["email"]
+  }).catch((e) => next(e));
+
+   for (const user of staff) {
+     staffMails.push(user.email);
+   }
+
+  const message = `
+    ${applicant.character} (${applicant.discord}) a posté sa candidature sur le site !
+  `
+  for (const staffMail of staffMails) {
+    const mail = mailTemplate(staffMail, "Une nouvelle candidature est disponible sur Namazu Wasshoi !", message);
+    transport.sendMail(mail);
   }
 
-  Applicant.create(applicant)
-  .then((applicant) => {
-    const staffMails = process.env.STAFF_MAILS.split(",");
-    const message = `
-      ${applicant.character} (${applicant.discord}) a posté sa candidature sur le site !
-    `
-    for (const staffMail of staffMails) {
-      const mail = mailTemplate(staffMail, "Une nouvelle candidature est disponible sur Namazu Wasshoi !", message);
-      transport.sendMail(mail);
-    }
-
-    res.status(201).json({ applicant });
-  })
-  .catch(() => res.status(400).json({ error: "Une erreur s'est produite. Si vous aviez déjà postulé, vous ne pourrez pas le faire une seconde fois. " }));
+  res.status(201).json({ message: "Merci ! Ta candidature a bien été envoyée." });
 });
 
 //Delete applicant
