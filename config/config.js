b2f7b639c7825f2d1f6ae9ef80aadd90066c3be4
EmilieGonin <emilie.yuu@gmail.com>
2021-09-13 17:29:47 +0200

    build(deps): replace mysql2 with mariadb

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.LOCAL_DATABASE_URL,
-    dialect: "mysql",
+    dialect: "mariadb",
     logging: false,
     timezone: "Europe/Paris"
   },
   production: {
     url: process.env.JAWSDB_MARIA_URL,
-    dialect: "mysql",
+    dialect: "mariadb",
     logging: false,
     timezone: "Europe/Paris"
   }
