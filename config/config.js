522f950143d19e3033940dbda2439283631aa65f
EmilieGonin <emilie.yuu@gmail.com>
2021-09-13 16:56:30 +0200

    feat: add Europe/Paris timezone to sequelize config

--- config/config.js
+++ config/config.js
@@ -4,11 +4,13 @@ module.exports = {
   development: {
     url: process.env.LOCAL_DATABASE_URL,
     dialect: "mysql",
-    logging: false
+    logging: false,
+    timezone: "Europe/Paris"
   },
   production: {
     url: process.env.JAWSDB_MARIA_URL,
     dialect: "mysql",
-    logging: false
+    logging: false,
+    timezone: "Europe/Paris"
   }
 }
