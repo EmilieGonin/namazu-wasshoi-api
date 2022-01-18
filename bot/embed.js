2b6993c652a3b4da3fbe8e5d5fd30cb8a637c54a
EmilieGonin <emilie.yuu@gmail.com>
2022-01-18 17:06:22 +0100

    feat: add !planning command with embed messages

---
+++ bot/embed.js
@@ -0,0 +1,32 @@
+const embed = {
+  color: "#e62e39",
+  title: "Test"
+}
+
+const planning = {
+  cartes: {
+    color: "#ffd700",
+    title: "🔹Cartes aux trésors",
+    description: "Essayez de récupérer votre carte journalière, ou au moins d'en apporter une.",
+    // thumbnail: {
+    //   url: "https://i.goopics.net/fc2ntk.png"
+    // },
+    image: {
+      url: "attachment://cartes.png"
+    },
+    fields: []
+  },
+  atelier: {
+    color: "#ffd700",
+    title: "🔹Cartes aux trésors",
+    description: "Essayez de récupérer votre carte journalière, ou au moins d'en apporter une.",
+    // thumbnail: {
+    //   url: "https://i.goopics.net/fc2ntk.png"
+    // },
+    image: {
+      url: "attachment://cartes.png"
+    }
+  },
+}
+
+module.exports = { planning, embed };
--- bot/gyoshoi.js
+++ bot/gyoshoi.js
@@ -1,18 +1,86 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed, MessageAttachment } = require('discord.js');
+const { planning, embed } = require('./embed');
 const token = process.env.WASSHOBOT_KEY;
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
 });
 
-client.once("ready", () => {
-  console.log("Je suis prêt, wasshoi !");
+const roles = {
+  officier: '674204890054131712',
+  test: '932934305552941066'
+}
+
+client.once('ready', () => {
+  console.log('Je suis prêt, wasshoi !');
 })
 
-client.on("messageCreate", msg => {
+client.on('messageCreate', msg => {
   const string = msg.content.toLowerCase();
-  if (!msg.author.bot && (string.includes("wasshoi") && string.includes("gyoshoi"))) {
-    msg.reply("Yes yes, wasshoi !");
+  if (!msg.author.bot && (string.includes('wasshoi') && string.includes('gyoshoi'))) {
+    msg.reply('Yes yes, wasshoi !');
+  }
+})
+
+//Planning
+client.on('messageCreate', msg => {
+  const isAdmin = msg.member.roles.cache.has(roles.test);
+  // const isAdmin = true;
+  const string = msg.content.toLowerCase();
+  if (!msg.author.bot && isAdmin && string.includes('!planning')) {
+    const type = string.split(' ')[1];
+    const date = string.split(' ')[2];
+    const hour = string.split(' ')[3];
+    console.log(type);
+    console.log(date);
+    console.log(hour);
+    const file = new MessageAttachment('./assets/' + type + '.png');
+
+    if (!type || !date || !hour) {
+      msg.reply(':warning: Veuillez préciser un type de sortie, une date et une heure de départ.\n\n🔹**Exemple :** `!planning cartes 01/01/2022 21`')
+      .then(() => {
+        msg.delete();
+      })
+    } else if (!planning[type]) {
+      const types = [];
+      for (let type in planning) {
+        types.push(' ' + type)
+      }
+
+      msg.reply(':warning: Le type de sortie "' + type +'" est incorrect.\n\n🔹**Liste des types acceptés :**' + types.toString())
+      .then(reply => {
+        msg.delete();
+      })
+    } else {
+      planning[type].fields.unshift(
+        { name: ':calendar: Date', value: '`' + date + '`', inline: true },
+        { name: ':clock1: Heure de départ', value: '`' + hour + '`', inline: true }
+      );
+
+      // const emojiTank = client.emojis.get()
+
+      planning[type].fields.push(
+        { name: '** **', value: '<:Tank:674261754225754152> **Tanks**', inline: true },
+        { name: ':Healer: **Healers**', value: '** **', inline: true }
+      );
+
+      msg.delete();
+      msg.channel.send({ embeds: [planning[type]], files: [file] })
+      .then(msg => {
+        msg.react('901253049077612584');
+
+        const filter = (reaction, user) => {
+          return ['901253049077612584'].includes(reaction.emoji.id) && !user.bot;
+        };
+
+        const collector = msg.createReactionCollector({ filter, time: 10000});
+
+        collector.on('collect', (reaction, user) => {
+          console.log(reaction.emoji.name);
+          console.log(user.username);
+        });
+      })
+    }
   }
 })
 
