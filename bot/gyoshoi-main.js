5953bbe86d513b99d6b186b903240c63c87d69ec
EmilieGonin <emilie.yuu@gmail.com>
2022-08-31 19:59:47 +0200

    refactor: comment destroyall command

--- bot/gyoshoi-main.js
+++ bot/gyoshoi-main.js
@@ -1,15 +1,15 @@
 const { confirm } = require('./functions');
 
 module.exports = main = {
-  destroyall: (msg) => {
-    confirm(msg, '!shoi destroyall').then(confirmed => {
-      if (confirmed) {
-        msg.channel.bulkDelete(100);
-      } else {
-        msg.delete();
-      }
-    })
-  },
+  // destroyall: (msg) => {
+  //   confirm(msg, '!shoi destroyall').then(confirmed => {
+  //     if (confirmed) {
+  //       msg.channel.bulkDelete(100);
+  //     } else {
+  //       msg.delete();
+  //     }
+  //   })
+  // },
   test: () => {
     console.log('test');
   }
